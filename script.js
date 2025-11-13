e1f9c82cff1585866b0fa8b932220c4b868f8dae
roox0262 <Mertcankay8@gmail.com>
2025-11-13 05:49:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ function showContent(section) {
     document.getElementById(section).style.display = 'block';
 }
 
+// Yeni sekmede URL açma
+function openInNewTab(url) {
+    window.open(url, '_blank');
+}
+
 function copyAPI(url) {
     navigator.clipboard.writeText(url)
         .then(() => alert('API kopyalandı: ' + url))
         .catch(err => alert('Kopyalama hatası: ' + err));
 }
 
+// Telegram butonları
+function goToLink(url) {
+    window.open(url, '_blank');
+}
+
+// Örnek test API
 function testAPI(url) {
     fetch(url)
         .then(res => res.json())
         .then(data => alert('API Yanıtı: ' + JSON.stringify(data)))
         .catch(err => alert('API Hatası: ' + err));
-}
-
-// Telegram butonları
-function goToLink(url) {
-    window.open(url, '_blank');
 }
